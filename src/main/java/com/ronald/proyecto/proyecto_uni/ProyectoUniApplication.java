d3e479b2e04f5c3d7ac4db7ae636b92d8ccefb42
Ronald <ronald_cs14@hotmail.com>
2025-04-17 14:10:33 -0500

    empezando...

---
+++ src/main/java/com/ronald/proyecto/proyecto_uni/ProyectoUniApplication.java
@@ -0,0 +1,13 @@
+package com.ronald.proyecto.proyecto_uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronald/proyecto/proyecto_uni/ProyectoUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronald.proyecto.proyecto_uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
